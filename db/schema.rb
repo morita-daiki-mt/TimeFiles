4cb358548118324eed0eae408bac6435ce96c5a5
morita daiki <morita.daiki.works@gmail.com>
2020-07-23 16:52:49 +0900

    add first user, add task model controller and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_071253) do
+ActiveRecord::Schema.define(version: 2020_07_23_074151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "content"
+    t.string "icon"
+    t.text "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
