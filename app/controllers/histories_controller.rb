16f954ccbcce5ac26716ca2992508b0f9eeb2d07
morita daiki <morita.daiki.works@gmail.com>
2020-07-27 16:52:01 +0900

    controller仮

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,2 +1,51 @@
 class HistoriesController < ApplicationController
+  def index
+    @histories = History.all
+  end
+
+  def show
+    @history = History.find(params[:id])
+  end
+
+  def new
+    @history = History.new
+  end
+
+  def create
+    @history = History.new(history_params)
+
+    if @history.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @history = History.find(params[:id])
+  end
+
+  def update
+    @history = History.find(params[:id])
+
+    if @history.update(history_params)
+      redirect_to tasks_path
+    else
+      render :show
+    end
+  end
+
+  def destroy
+    @history = History.find(params[:id])
+    @history.destroy
+  
+    redirect_to tasks_path
+  end
+
+  private
+
+  def history_params
+    params.require(:history).permit(:action_at)
+  end
+  
 end
