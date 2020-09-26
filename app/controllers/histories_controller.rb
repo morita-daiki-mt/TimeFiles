dfc42ba7c60cc634c1e841f8b31772fe6f2383c3
morita daiki <morita.daiki.works@gmail.com>
2020-09-26 17:01:19 +0900

    todayボタンをとりあえず設置。起動済み

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -5,7 +5,7 @@ class HistoriesController < ApplicationController
     @history = current_user.histories.build(history_params)
     if @history.save
       flash[:success] = '実行日を追加しました'
-      redirect_to task_path(id: @history.task_id)
+      redirect_back(fallback_location: root_path)
     else
       flash[:danger] = '実行日追加に失敗しました'
       # render task_path(id: @history.task_id)
