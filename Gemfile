8194934d1ddefc3941b2b619e092f7bdea781f6b
morita daiki <morita.daiki.works@gmail.com>
2020-10-05 16:34:32 +0900

    ログイン時のテスト追加

--- Gemfile
+++ Gemfile
@@ -53,6 +53,9 @@ group :development, :test do
   gem 'rspec-rails'
   gem "factory_bot_rails"
   gem 'faker'
+  gem 'capybara'
+  gem 'selenium-webdriver'
+
 end
 
 group :development do
--- spec/requests/devise_users_spec.rb
+++ spec/requests/devise_users_spec.rb
@@ -4,11 +4,14 @@ RSpec.describe "UserAuthentications", type: :request do
   let(:user) { create(:user) }
   let(:user_params) { attributes_for(:user) }
   let(:invalid_user_params) { attributes_for(:user, email: "") }
+  let!(:login_user) do
+    create(:user,
+            email: 'test@example.com',
+            password: '123456',
+            password_confirmation: '123456')
+  end
 
-  describe 'POST #create' do
-    before do
-      ActionMailer::Base.deliveries.clear
-    end
+  describe 'アカウント作成' do
     context 'パラメータが妥当な場合' do
       it 'リクエストが成功すること' do
         post user_registration_path, params: { user: user_params }
@@ -19,6 +22,10 @@ RSpec.describe "UserAuthentications", type: :request do
         expect do
           post user_registration_path, params: { user: user_params }
         end.to change(User, :count).by 1
+      end
+
+      it 'パスワードが暗号化されていること' do
+        expect(user.password).to_not eq "password"
       end
 
       it 'リダイレクトされること' do
@@ -37,6 +44,28 @@ RSpec.describe "UserAuthentications", type: :request do
         expect do
           post user_registration_path, params: { user: invalid_user_params }
         end.to_not change(User, :count)
+      end
+    end
+  end
+
+  describe 'ログイン' do
+    context 'パラメーターが妥当な場合' do
+      it 'ログインに成功すること' do
+        visit new_user_session_path
+        fill_in 'Eメール', with: 'test@example.com'
+        fill_in 'パスワード', with: '123456'
+        click_button 'ログイン'
+        expect(page).to have_button 'ADD NEW TASK'
+      end
+    end
+
+    context 'パラメーターが不正な場合' do
+      it 'ログインに失敗すること' do
+        visit new_user_session_path
+        fill_in 'Eメール', with: 'test@example.com'
+        fill_in 'パスワード', with: ''
+        click_button 'ログイン'
+        expect(page).to have_button 'ログイン'
       end
     end
   end
