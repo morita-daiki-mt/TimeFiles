c85c3f0c840489f3475b5c8f9bd17e5d3483245f
morita daiki <morita.daiki.works@gmail.com>
2020-10-11 15:22:57 +0900

    ゲストユーザーを削除できないようにする

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
