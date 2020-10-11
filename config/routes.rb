bd376452d24ecebdcafd0713906fb32479bca8e5
morita daiki <morita.daiki.works@gmail.com>
2020-10-11 15:20:28 +0900

    guest追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
 
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   resources :tasks do
     resources :histories
   end
