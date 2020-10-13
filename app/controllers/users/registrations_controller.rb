ce409f2b24fff2176a67c5da13abb0628e941a0f
morita daiki <morita.daiki.works@gmail.com>
2020-10-13 17:53:23 +0900

    ログイン中にroot_pathを開こうとするとtasks_pathが開かれるよう

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def check_guest
     if resource.email == 'guest@example.com'
-      redirect_to root_path, alert: 'ゲストユーザーの変更・削除はできません。'
+      redirect_to rootpath, alert: 'ゲストユーザーの変更・削除はできません。'
     end
   end
 
