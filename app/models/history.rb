a2eed1c2e75da7f26d0d91fc78b19b0f33001b1a
morita daiki <morita.daiki.works@gmail.com>
2020-11-16 18:15:05 +0900

    user,task_idも追加

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,6 +1,8 @@
 class History < ApplicationRecord
   belongs_to :task
   belongs_to :user
+  validates :user_id, presence: true
+  validates :task_id, presence: true
   validates :action_at, presence: { message: "日付を登録してください" }, uniqueness: { scope: :task_id, massage: "同じ日付は登録できません" }
   validate :action_at_be_in_the_future
 
