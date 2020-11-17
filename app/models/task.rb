bc390fe50af51cae3ac6e835438d027bd4f4e07b
morita daiki <morita.daiki.works@gmail.com>
2020-11-17 17:01:35 +0900

    showボタン改善

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,6 @@ class Task < ApplicationRecord
   has_many :histories, dependent: :destroy
   accepts_nested_attributes_for :histories
   mount_uploader :icon, ImageUploader
-  validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 20 }
   validates :memo, length: { maximum: 400 }
 end
