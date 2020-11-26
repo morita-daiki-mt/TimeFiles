c92854ad191ab6cdc457b838595ad10c5cae00ec
morita daiki <morita.daiki.works@gmail.com>
2020-11-26 17:01:33 +0900

    model側でaction_atを降順にした

--- app/models/history.rb
+++ app/models/history.rb
@@ -2,6 +2,7 @@ class History < ApplicationRecord
   belongs_to :task
   validates :action_at, presence: { message: "日付を登録してください" }, uniqueness: { scope: :task_id, massage: "同じ日付は登録できません" }
   validate :action_at_be_in_the_future
+  default_scope -> { order(action_at: :desc) }
 
   def action_at_be_in_the_future
     if action_at.present? && action_at > Date.today
