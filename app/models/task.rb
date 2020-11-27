81ab6f02c6d5249398222871b7b72c15878459b1
morita daiki <morita.daiki.works@gmail.com>
2020-11-27 15:12:36 +0900

    task名かぶり防止

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,6 @@ class Task < ApplicationRecord
   has_many :histories, dependent: :destroy
   accepts_nested_attributes_for :histories
   mount_uploader :icon, ImageUploader
-  validates :content, presence: true, length: { maximum: 20 }
+  validates :content, presence: true, length: { maximum: 20 }, uniqueness: { scope: :user_id, massage: "同じタスク名は登録できません" }
   validates :memo, length: { maximum: 400 }
 end
